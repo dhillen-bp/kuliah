705e41e226b26e6c0be671389cb47458548edafa
dhillenbp179 <102419678+dhillenbp179@users.noreply.github.com>
2022-03-27 09:54:34 +0700

    Delete semester-4/pemrograman-web/crud directory

--- semester-4/pemrograman-web/crud/insert.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "connect.php";
-
-$nama = $_POST['nama'];
-
-$mysql = "INSERT INTO mahasiswa (nama) VALUES('$nama')";
-if (mysqli_query($conn, $mysql)) {
-    echo "data berhasil ditambahkan!";
-}
