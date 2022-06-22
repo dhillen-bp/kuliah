d2736d240181ea27a2406afc9c0948c2b0e86b89
dhillenbp179 <102419678+dhillenbp179@users.noreply.github.com>
2022-06-22 19:58:18 +0700

    Create GajiDosenInter.java

---
+++ semester-4/pemrograman-java/responsi/GajiDosenInter.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public interface GajiDosenInter {
+    
+    public double gajiGolongan();
+    public double gajiStruktural();
+    public double gajiFungsional();
+    public double TotalGajiDosen();
+    public double mengajar();
+    
+}
